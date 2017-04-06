391fd46a3b254bcb36a4d783e10e0bf5ab9ed336
Gagik <alixanyangagik@gmail.com>
2017-04-06 21:22:20 +0400

    add function clear

--- js/script.js
+++ js/script.js
@@ -2,47 +2,53 @@
 (function IIFE() {
     var board = {
         grid: null,
-        value: null,
-        btn: null,
-        input: null,
-        form:null
+        form: null,
+        container: null
     };
 
     function init() {
-        board.btn = document.getElementById('submit');
+        board.container = document.getElementById('container');
         board.grid = document.getElementById('grid');
-        board.input = document.getElementById('input');
-        board.form=document.getElementById('form');
+        board.form = document.getElementById('form');
         bindEvents();
     }
 
     function bindEvents() {
-        board.form.addEventListener("submit", function (event) {
-            event.preventDefault();
-            draw();
-        });
+        if (board.form) {
+            if (board.form.addEventListener) {
+                board.form.addEventListener("submit", draw)
+            } else if (board.form.attachEvent) {
+                board.form.addEventListener("onsubmit", draw)
+            }
+        }
+    }
+
+    function clearGrid() {
+        board.container.innerHTML = '';
+    }
+
+    function clearInput(input) {
+        input.value = '';
     }
 
     function draw(event) {
-
-        board.value = document.getElementById('input').value;
-        var number = parseInt(board.value);
-        if (isNaN(number)) {
-            alert("Please type number ,instead of string")
+        event.preventDefault();
+        var inputElem = document.getElementById('input');
+        var pareNumber = parseInt(inputElem.value);
+        if (isNaN(pareNumber)) {
+            alert("Please type number ,instead of string");
+            return;
         }
-
-        var container = document.getElementById("container");
-        container.innerHTML = "";
-        board.input.value = "";
-        for (var i = 0; i < board.value; i++) {
+        clearGrid();
+        for (var i = 0; i < pareNumber; i++) {
             var grid = document.createElement("div");
-            container.appendChild(grid);
+            board.container.appendChild(grid);
             grid.id = "container";
-            for (var j = 0; j < board.value; j++) {
+            for (var j = 0; j < pareNumber; j++) {
                 grid.innerHTML += "<div id='divInContainer'></div>"
             }
         }
-
+        clearInput(inputElem)
     }
 
     init();
